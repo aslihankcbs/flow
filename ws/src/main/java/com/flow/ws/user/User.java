40eccbe02e4a3592f1c1c4dd4b90ba18fd051476
Aslıhan Kocabaş <kocabasasli66@gmail.com>
2021-02-20 01:55:04 +0300

    Kullanıcı kaydı için component oluşturuldu. User dto ve restcontroller oluştuldu.

---
+++ ws/src/main/java/com/flow/ws/user/User.java
@@ -0,0 +1,19 @@
+package com.flow.ws.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String displayName;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", displayName='" + displayName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
